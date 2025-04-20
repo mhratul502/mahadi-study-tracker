f811934f79dd206159f3adb78637c5f4479b3a51
mhratul502 <ytmh59740@gmail.com>
2025-04-20 23:25:46 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = "study-tracker-v1";
+const filesToCache = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
